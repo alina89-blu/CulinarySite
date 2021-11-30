b1949521b35aec7c0401602db01d1526d87d0e28
Alina Bludaru <alinocica.89@mail.ru>
2021-11-30 14:50:01 +0200

    Added Project  ServiceLayer

---
+++ CulinarySite/ServiceLayer/AddressService.cs
@@ -0,0 +1,49 @@
+﻿using Database;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public class AddressService : IAddressService
+    {
+        private readonly IReadOnlyGenericRepository<Address> addressReadOnlyRepository;
+        private readonly IWriteGenericRepository<Address> addressWriteRepository;
+
+        public AddressService(
+            IReadOnlyGenericRepository<Address> addressReadOnlyRepository, 
+            IWriteGenericRepository<Address> addressWriteRepository)
+        {
+            this.addressReadOnlyRepository = addressReadOnlyRepository;
+            this.addressWriteRepository = addressWriteRepository;
+        }
+
+        public void CreateAddress(Address address)
+        {
+            this.addressWriteRepository.Create(address);
+            this.addressWriteRepository.Save();
+        }
+        public void UpdateAddress(Address address)
+        {
+            this.addressWriteRepository.Update(address);
+            this.addressWriteRepository.Save();
+        }
+        public void DeleteAddress(int id)
+        {
+            this.addressWriteRepository.Delete(id);
+            this.addressWriteRepository.Save();
+
+        }        
+        public IEnumerable<Address> GetAddressList()
+        {
+            return this.addressReadOnlyRepository.GetItemList();
+        }
+        public Address GetAddress(int id)
+        {
+            return this.addressReadOnlyRepository.GetItem(id);
+        }
+    }
+}
---
+++ CulinarySite/ServiceLayer/IAddressService.cs
@@ -0,0 +1,16 @@
+﻿
+using Database;
+using System.Collections.Generic;
+
+namespace ServiceLayer
+{
+    public interface IAddressService
+    {
+        void CreateAddress(Address address);
+        void UpdateAddress(Address address);
+        void DeleteAddress(int id);      
+        IEnumerable<Address> GetAddressList();
+        Address GetAddress(int id);
+        
+    }
+}
