f8489a35f8a7a40d31bcc9edcc77deb4b5d5b6e2
Alina Bludaru <alinocica.89@mail.ru>
2021-12-01 15:39:54 +0200

    Created CulinarySiteApp

--- CulinarySiteApp/src/app/app.component.spec.ts
+++ CulinarySiteApp/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'culinarySite-app'`, () => {
+  it(`should have as title 'CulinarySiteApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('culinarySite-app');
+    expect(app.title).toEqual('CulinarySiteApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('culinarySite-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CulinarySiteApp app is running!');
   });
 });
